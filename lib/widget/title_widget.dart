ca3779d048c44c906be6c23a94253d428d1ed722
MD Ruhulamin Khan <skipperuzumaki@gmail.com>
2021-11-07 20:22:20 +0530

    added icon title

--- lib/widget/title_widget.dart
+++ lib/widget/title_widget.dart
@@ -19,3 +19,31 @@ class TitleWidget extends StatelessWidget {
     textAlign: TextAlign.center,
   );
 }
+
+class TitleImageWidget extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const TitleImageWidget({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Column(
+    children: [
+      Icon(icon, size: 100, color: Colors.white),
+      const SizedBox(height: 16),
+      Text(
+        text,
+        style: TextStyle(
+          fontSize: 42,
+          fontWeight: FontWeight.w400,
+          color: Colors.white,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    ],
+  );
+}
